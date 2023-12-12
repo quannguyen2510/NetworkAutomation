4ef856ec13a77c2f468337632972ef616328a94d
quannguyen2510 <nguyenanhquan2510@gmail.com>
2023-12-12 20:54:08 +0700

    12/12 (2)

--- networking.py
+++ networking.py
@@ -1,4 +1,3 @@
-
 from netmiko import ConnectHandler
 
 f = open('switch_ip_list.txt')
@@ -8,19 +7,21 @@ all_devices = []
 
 for ip in ip_list:
     R = {
-        "device_type" : "cisco_ios",
-        "ip" : ip,
-        "username" : "root",
-        "password" : "quan"
+        "device_type": "cisco_ios",
+        "ip": ip,
+        "username": "root",
+        "password": "quan"
     }
     all_devices.append(R)
 
-
 for switch in all_devices:
-    net_connect = ConnectHandler(**switch)
-    output = net_connect.send_command("show run | i hostname")
+    try:
+        net_connect = ConnectHandler(**switch)
+        output = net_connect.send_command("show run | i hostname")
 
-    print(output)
-    print('-------')
+        print(output)
+        print('-------')
 
-
+        net_connect.disconnect()  # Disconnect after each successful connection
+    except Exception as e:
+        print(f"Failed to connect to {switch['ip']} with error: {str(e)}")
